20d18eb3e6a8177c8715cf8bc8e1d1471e11cd7f
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-14 19:48:38 +0600

    mongoose validation error implement with global error handler commit

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,11 +2,12 @@ import { ErrorRequestHandler } from 'express';
 import {ZodError} from 'zod';
 import { TErrorSources } from '../interface/error';
 import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = 'Something went wrong!';
-  let errorSource: TErrorSources = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong.',
@@ -17,6 +18,13 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSource = simplifiedError?.errorSources;
+    errorSources = simplifiedError?.errorSources;
+  } else if(err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if(err?.name === "CastError") {
+    console.log("this is the cast error.");
   }
 };
