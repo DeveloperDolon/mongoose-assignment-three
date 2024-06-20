a5d83ff68354958a10ef3075c50d075ccacb3407
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-20 12:02:33 +0600

    .md file edit commitI

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+import cors from 'cors';
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to assignment 3 server!');
